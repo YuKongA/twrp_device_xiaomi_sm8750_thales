55be6272ee14fe0f665d73658563295f75142d47
YuKongA <70465933+YuKongA@users.noreply.github.com>
2025-11-11 11:33:32 +0800

    6

--- recovery/root/odm/bin/variant-files-copy.sh
+++ recovery/root/odm/bin/variant-files-copy.sh
@@ -5,10 +5,8 @@ variant=$(getprop ro.boot.hardware.sku)
 
 cp -rf /vendor/variant/$variant/vendor/* /vendor
 cp -rf /vendor/variant/$variant/odm/* /odm
-cp -rf /vendor/variant/$variant/system/* /system
 chmod -R 755 /vendor/bin/*
 chmod -R 755 /odm/bin/*
-chmod 755 /system/bin/se_omapi
 
 resetprop variant.files_copied "1"
 
